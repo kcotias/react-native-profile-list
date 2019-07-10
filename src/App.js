0a20cc3c9a9c14be44a17e079300431a6f579b5b
kcotias <kaianexperia@gmail.com>
2019-07-10 15:39:33 -0300

    remove boilerplate and add project folder structure

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const App = props => (
+  <View>
+    <Text>App</Text>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+});
+
+export default App;
