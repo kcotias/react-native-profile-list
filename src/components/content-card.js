0d49a0fd680710feb93f4fd6c9cc6484f13396d7
kcotias <kaianexperia@gmail.com>
2019-07-10 23:09:36 -0300

    add content card component/ finish service screen

---
+++ src/components/content-card.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import colors from '../config/colors';
+
+
+const ContentCard = (props) => {
+
+    return (
+        <View
+            style={styles.container}
+        >
+            <View style={styles.imgWrapper}>
+                <Image
+                    source={props.foto}
+                    resizeMode="contain"
+                    style={styles.img}
+                />
+            </View>
+            <View style={{ marginLeft: 15 }}>
+                <View style={{ flexDirection: 'row', marginTop: 5 }}>
+                    <Text style={styles.prefix}>NOME:</Text>
+                    <Text style={styles.content}>{props.nome}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', marginTop: 5 }}>
+                    <Text style={styles.prefix}>CARGO:</Text>
+                    <Text style={styles.content}>{props.cargo}</Text>
+                </View>
+                <View style={{ flexDirection: 'row', marginTop: 5 }}>
+                    <Text style={styles.prefix}>IDADE:</Text>
+                    <Text style={styles.content}>{props.idade}</Text>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: 'white',
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 15,
+        marginBottom: 10,
+        shadowColor: 'gray',
+        shadowOpacity: 0.5,
+        shadowOffset: {
+            height: 2,
+            width: 2
+        },
+        shadowRadius: 3,
+        elevation: 3,
+    },
+    img: {
+        height: 80,
+        width: 80,
+        alignItems: 'flex-end'
+    },
+    sticker: {
+        height: 22,
+        width: 22,
+        borderRadius: 20,
+        backgroundColor: colors.secondary,
+        marginTop: 2,
+        marginRight: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    stickerLabel: {
+        color: colors.primary,
+        fontWeight: '700',
+        fontSize: 12
+    },
+    prefix: {
+        fontWeight: '300',
+        color: colors.font,
+        marginRight: 5
+    },
+    content: {
+        fontWeight: '600',
+        color: colors.font
+    },
+    imgWrapper: {
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+
+});
+
+export default ContentCard;
--- src/screens/service-screen.js
+++ src/screens/service-screen.js
@@ -1,17 +1,72 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
+import Header from '../components/header';
+import LIST_DATA from '../utils/constants';
+import ListCard from '../components/list-card';
+import ContentCard from '../components/content-card';
 
+const ServiceScreen = () => {
 
-const ServiceScreen = (props) => (
-    <View>
-        <Text>ServiceScreen</Text>
-    </View>
-);
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [idade, setIdade] = useState('');
+    const [foto, setFoto] = useState('');
+
+    const _renderItem = (item, index) => (
+        <ListCard
+            nome={item.nome}
+            cargo={item.cargo}
+            foto={item.foto}
+            onPress={() => {
+                setNome(item.nome);
+                setCargo(item.cargo);
+                setIdade(item.idade);
+                setFoto(item.foto);
+            }}
+            index={index}
+        />
+    );
+
+    const handlePress = (item) => {
+        this.props.navigation.navigate('PROFILE',
+            {
+                nome: item.nome,
+                cargo: item.cargo,
+                foto: item.foto,
+                idade: item.idade
+            });
+    }
+
+    return (
+        <View style={styles.container}>
+            <View style={{ flex: 1 }}>
+                <Header />
+                <ContentCard
+                    nome={nome}
+                    cargo={cargo}
+                    idade={idade}
+                    foto={foto}
+                />
+            </View>
+            <View style={{ flex: 2 }}>
+                <FlatList
+                    style={styles.flatlist}
+                    data={LIST_DATA}
+                    renderItem={({ item, index }) => _renderItem(item, index)}
+                />
+            </View>
+        </View>
+    );
+}
+
 
 const styles = StyleSheet.create({
     container: {
         flex: 1
     },
+    flatlist: {
+        paddingHorizontal: 20,
+    }
 
 });
 
